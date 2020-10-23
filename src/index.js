5ed94d44d57068ede30267cb5864586490d7f348
Rafal Nowak <rafal.robi.nowak@gmail.com>
2020-10-23 11:54:55 +0200

    Preparing file for rendering React Components

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = ()  => {
+  return <h1>movie List project</h1>;
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
